81292a7139e35c968155a0c9d53113b9d06a0541
Rok Samsa <rok.samsa@gmail.com>
2024-09-04 22:18:16 +0200

    Fix: Few addons

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,21 +2,33 @@ import React from "react";
 
 import styles from "./Menu.module.scss";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Menu = () => {
+  const pathname = usePathname();
+
   return (
     <div className={styles.menu}>
       <ul className={styles.menuList}>
-        <li className={styles.menuItem}>
-          <Link href="/about">Home</Link>
+        <li
+          className={`${styles.menuItem} ${
+            pathname === "/app" ? styles.active : ""
+          }`}
+        >
+          <Link href="/dashboard">Dashboard</Link>
         </li>
-        <li className={styles.menuItem}>
-          <Link href="/about">Dashboard</Link>
+        <li
+          className={`${styles.menuItem} ${
+            pathname === "/settings" ? styles.active : ""
+          }`}
+        >
+          <Link href="/settings">Settings</Link>
         </li>
-        <li className={styles.menuItem}>
-          <Link href="/about">Settings</Link>
-        </li>
-        <li className={styles.menuItem}>
+        <li
+          className={`${styles.menuItem} ${
+            pathname === "/about" ? styles.active : ""
+          }`}
+        >
           <Link href="/about">About</Link>
         </li>
       </ul>
